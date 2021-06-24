63aa22b8c4417f2fc877800a6e5bac5269ccdd73
Yurun <admin@yurunsoft.com>
2021-06-24 10:02:38 +0800

    Add declare_strict_types to .php_cs.dist

--- examples/consumer.php
+++ examples/consumer.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use longlang\phpkafka\Consumer\ConsumeMessage;
 use longlang\phpkafka\Consumer\Consumer;
 use longlang\phpkafka\Consumer\ConsumerConfig;
--- examples/producer.php
+++ examples/producer.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use longlang\phpkafka\Producer\Producer;
 use longlang\phpkafka\Producer\ProducerConfig;
 
